62ada217d39d27f0034b53ca77262ca0cff2f7e3
Karina Klages <karinaklages@web.de>
2025-09-26 13:24:41 +0200

    refactor: dialog added

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// Variables
+
+// DOM Manipulation
+const orderDialogRef = document.getElementById ("orderDialog");
+
+// State
+index = 0;
+
+
+
+// Functions
+
 function init() {
   
+}
+
+
+function openDialog() {
+    orderDialogRef.showModal();
+    document.body.style.overflow = "hidden"; // Scrollbar hidden
+}
+
+
+function closeDialog() {
+    orderDialogRef.close();
+    document.body.style.overflow = "";
 }
