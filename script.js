7a81e058b0c51e7ffc8f482a20f13c5855bc5582
Karina Klages <karinaklages@web.de>
2025-09-29 13:05:23 +0200

    feat: content render function added

--- script.js
+++ script.js
@@ -7,17 +7,37 @@ const orderDialogRef = document.getElementById ("orderDialog");
 index = 0;
 
 
-
 // Functions
 
 function init() {
-  
+    renderDishesContent();
+}
+
+
+function renderDishesContent() {
+    const dishesContentRef = document.getElementById("dishesContent");
+    dishesContentRef.innerHTML = "";
+
+    // Renders category names using Object.keys() to return an array
+    for (let indexCategory = 0; indexCategory < Object.keys(dishes.dishes).length; indexCategory++) {
+        const categoryName = Object.keys(dishes.dishes)[indexCategory];
+        dishesContentRef.innerHTML += getDishesContent(categoryName);
+
+    const dishesCardContentRef = document.getElementsByClassName("cardWrapper");
+    const currentCardWrapper = dishesCardContentRef[dishesCardContentRef.length - 1];
+
+        // Renders cards
+        for (let index = 0; index < dishes.dishes[categoryName].length; index++) {
+            const card = dishes.dishes[categoryName][index];
+            currentCardWrapper.innerHTML += getDishesCardContent(card);
+        }
+    }
 }
 
 
 function openDialog() {
     orderDialogRef.showModal();
-    document.body.style.overflow = "hidden"; // Scrollbar hidden
+    document.body.style.overflow = "hidden"; // Hides the scrollbar
 }
 
 
---
+++ scripts/templates.js
@@ -0,0 +1,22 @@
+function getDishesContent(categoryName) {
+    return `
+        <h2>${categoryName}</h2>
+        <div class="cardWrapper"></div>
+        <div class="space_area_height"></div>
+    `;
+}
+
+
+function getDishesCardContent(card) {
+    return `
+        <div class="dishes_card_content" tabindex="0">
+            <img class="card_content_img" src="${card.img}" alt="${card.alt}">
+            <div>
+                <h3>${card.title}</h3>
+                <p>${card.description}</p><br>
+                <p><b>${card.price.toFixed(2)} &euro;</b></p>
+            </div>
+            <img class="add_icon" src="./assets/icons/add-icon.png" alt="Gericht zum Warenkorb hinzufügen" aria-label="Gericht zum Warenkorb hinzufügen">
+        </div>
+    `;
+}
